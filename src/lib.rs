39648d4be956bd4b69ca6fde51c6c7575acffe9a
Nova <technobaboo@gmail.com>
2026-02-19 16:08:13 -0800

    fix: warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
-pub mod settings;
+#![allow(clippy::too_many_arguments, clippy::type_complexity)]
+
+pub mod histogram;
+pub mod naive;
 pub mod phase;
 pub mod pipeline;
 pub mod queue;
+pub mod settings;
 pub mod textures;
-pub mod naive;
-pub mod histogram;
 
 use bevy::prelude::*;
 
-pub use settings::{WboitSettings, HistogramWboitSettings};
 pub use naive::NaiveWboitPlugin;
+pub use settings::{HistogramWboitSettings, WboitSettings};
 
 /// Convenience plugin that enables naive WBOIT.
 /// Add `WboitSettings` to a camera entity to opt in.
